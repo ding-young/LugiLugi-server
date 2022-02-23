a2125dbb84fac65a9fce0e4c29211d191e6662aa
ding-young <lsyhime@snu.ac.kr>
2022-02-23 20:29:18 +0900

    Feat: project start

---
+++ src/main/kotlin/com/snutaek/lugilugiserver/LugiLugiServerApplication.kt
@@ -0,0 +1,11 @@
+package com.snutaek.lugilugiserver
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LugiLugiServerApplication
+
+fun main(args: Array<String>) {
+    runApplication<LugiLugiServerApplication>(*args)
+}
