5302cf373b36a1301597d10093947c226dc4588d
Seoyoung Lee <lsyhime@snu.ac.kr>
2022-07-30 19:35:19 +0900

    Feat: image s3 upload

---
+++ src/main/kotlin/com/snutaek/lugilugiserver/domain/image/api/ImageController.kt
@@ -0,0 +1,27 @@
+package com.snutaek.lugilugiserver.domain.image.api
+
+import com.snutaek.lugilugiserver.domain.image.dto.ImageDto
+import com.snutaek.lugilugiserver.domain.image.service.ImageService
+import com.snutaek.lugilugiserver.domain.user.model.User
+import com.snutaek.lugilugiserver.global.auth.CurrentUser
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestPart
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.multipart.MultipartFile
+
+@RestController
+@RequestMapping("api/v1/image")
+class ImageController(
+    private val imageService: ImageService
+) {
+    @RequestMapping("/profile/me/")  // Should it be in userController..?
+    @ResponseStatus(HttpStatus.CREATED)
+    fun uploadUserProfileImage(
+        @CurrentUser user: User,
+        @RequestPart image: MultipartFile,
+    ) : ImageDto.ImageUploadResponse {
+        return ImageDto.ImageUploadResponse(imageService.uploadUserProfileImage(image, user))
+    }
+}
---
+++ src/main/kotlin/com/snutaek/lugilugiserver/domain/image/service/ImageService.kt
@@ -0,0 +1,72 @@
+package com.snutaek.lugilugiserver.domain.image.service
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.s3.AmazonS3
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.model.ObjectMetadata
+import com.amazonaws.auth.AWSStaticCredentialsProvider
+import com.amazonaws.auth.BasicAWSCredentials
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.multipart.MultipartFile
+import com.snutaek.lugilugiserver.domain.user.model.User
+import com.snutaek.lugilugiserver.domain.user.repository.UserRepository
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Configuration
+class AWSConfiguration {
+    @Bean
+    fun assetS3Client(
+        @Value("\${aws.access-key}") accessKey: String,
+        @Value("\${aws.secret-key}") secretKey: String,
+        @Value("\${aws.s3.endpoint}") s3Endpoint: String
+    ): AmazonS3 {
+        return AmazonS3ClientBuilder
+            .standard()
+            .withCredentials(AWSStaticCredentialsProvider(BasicAWSCredentials(accessKey, secretKey)))
+            .withRegion(Regions.AP_NORTHEAST_2)
+            .build()
+    }
+}
+
+// honestly this is... S3 service
+@Service
+class ImageService(
+    private val userRepository: UserRepository
+) {
+    @Value("\${aws.access-key}")
+    final lateinit var accessKey: String
+    @Value("\${aws.secret-key}")
+    final lateinit var secretKey: String
+    @Value("\${aws.s3.bucket}")
+    lateinit var bucketName: String
+    @Value("\${aws.s3.endpoint}")
+    lateinit var s3Endpoint: String
+
+    @Autowired
+    lateinit var amazonS3: AmazonS3
+
+    fun upload(file: MultipartFile, folderName: String, fileToken: String): String {
+        val fileName = file.originalFilename
+        val keyName = "images/$folderName/$fileToken/$fileName"   // random uuid token = fileToken?
+        val inputStream = file.inputStream
+        val meta = ObjectMetadata()
+        meta.contentLength = inputStream.available().toLong()
+        meta.contentType = file.contentType
+
+        amazonS3.putObject(bucketName, keyName, inputStream, meta)
+        return "${s3Endpoint}/${keyName}"
+    }
+
+    fun uploadUserProfileImage(image: MultipartFile, user: User): String {
+        val randomToken = UUID.randomUUID().toString()
+        val imageS3url = upload(image, "user/${user.username}", randomToken)
+        // TODO refactor
+        user.profileS3ObjectKey = imageS3url
+        userRepository.save(user)
+        return imageS3url
+    }
+}
